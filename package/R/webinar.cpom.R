d883197e3a6d08f479fb6c519f7507124d91eb9a
Ronald Hochreiter <ron@hochreiter.net>
2016-10-25 09:35:54 +0200

    package documentation

---
+++ package/R/webinar.cpom.R
@@ -0,0 +1,15 @@
+#' Webinar - Contemporary Portfolio Optimization Modeling using R
+#'
+#' http://finance-r.com/cpom/
+#' 
+#' In the first part of this Webinar we review the most common ways to conduct 
+#' the task of portfolio optimization with R. After this introduction, which can 
+#' be of use for anyone interested in portfolio opimization in general, some 
+#' remarks on the modeling of portfolio problems will be addressed. In the second 
+#' part, a revolutionary way to model and solve portfolio optimization problems 
+#' using R will be shown. The basic idea of conceptualizing a new way to model 
+#' portfolio optimization problems is to build a portfolio optimization modeling 
+#' language on top of a generalized algebraic modeling language. By focussing on 
+#' a number of different modeling and optimization approaches, the Webinar is 
+#' going to provide new insights for a broad range of interested parties. 
+"_PACKAGE"
